c3a0dedac4980f341b37af83e54430d797129175
akaash67 <md.akash6724@gmail.com>
2022-01-02 14:21:57 +0600

    header responsive done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,7 +8,7 @@ import './Header.css';
 
 const Header = () => {
 
-    return (
+    return ( 
         <>
         {/* top header  */}
         <div className="top-header">
@@ -22,15 +22,15 @@ const Header = () => {
 
         {/* header  */}
 
-        <Navbar bg="" expand="lg" className="d-flex align-items-center justify-content-center px-1">
+        <Navbar bg="" expand="lg" className=" px-1 main-navbar">
           
-            <Navbar.Brand href="#home">
+            <Navbar.Brand href="#home" className="grid-logo">
             <img className="hfe-site-logo-img elementor-animation- lazyloaded" src="https://demo2wpopal.b-cdn.net/ecolive/wp-content/uploads/2021/10/logo.svg" alt="logo" data-ll-status="loaded" width="50%"/>
             </Navbar.Brand>
             
 
             
-        <form action="" className="mx-auto" style={{width: '50%'}}>
+        <form action="" className="mx-auto search-form grid-search-form">
           <div className="input-group border p-1">
             <input type="search" placeholder="Search Products..." aria-describedby="button-addon3" className="form-control bg-none border-0 bg-transparent"/>
             <div className="input-group-append border-0">
@@ -39,16 +39,16 @@ const Header = () => {
           </div>
           
         </form>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav d-flex justify-content-between align-items-center">
-              <Nav className="mx-auto">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="grid-menu border-0 outline-0"/>
+            <Navbar.Collapse id="basic-navbar-nav" className="" >
+              <Nav className="mx-auto navs">
                 <Nav.Item className="d-flex justify-content-center align-items-center fw-normal mx-2">
                     <i className="far fa-user"></i>
                     <p style={{textAlign: 'left', margin: '0'}}>Hello, <br />
                     <NavLink to="/login"  className="text-decoration-none text-black user-login">Log In</NavLink> or <NavLink to="signup" className="text-decoration-none text-black user-login">Register</NavLink>
                     </p>
                 </Nav.Item>
-                <Nav.Item className="mx-2">
+                <Nav.Item className="mx-2 d-flex justify-content-center align-items-center">
                 <button type="button" className="border-0 p-0 position-relative">
                 <i className="far fa-heart"></i>
                   <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill pill-bg">
@@ -57,7 +57,7 @@ const Header = () => {
                   </span>
                 </button>
                 </Nav.Item>
-                <Nav.Item className="mx-2">
+                <Nav.Item className="mx-2 d-flex justify-content-center align-items-center">
                 <button type="button" className="border-0 p-0 position-relative">
                 <i class="fal fa-shopping-bag"></i>
                   <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill pill-bg">
