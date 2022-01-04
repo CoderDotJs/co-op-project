ba117de92d95d3819425184c28381ef9d5260f27
akaash67 <md.akash6724@gmail.com>
2022-01-04 16:52:24 +0600

    changes the review

--- src/Components/CustormerReview/CustomerReview.js
+++ src/Components/CustormerReview/CustomerReview.js
@@ -62,7 +62,7 @@ const CustomerReview = () => {
             </div>
             <div>
               <q className="fs-normal fw-light text-muted d-block my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, eius!</q>
-              <h4>Md Akash</h4>
+              <h4>Tom Hanks</h4>
               <h6 style={{color: '#61CE70'}}>Front End Developer</h6>
             </div>
           </div>
@@ -70,60 +70,60 @@ const CustomerReview = () => {
       <SwiperSlide className="mb-4">
           <div>
             <div className="customer-img-div">
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" id="customer-img"/>
+              <img src="https://p2.piqsels.com/preview/876/356/902/boy-businessman-close-up-eyes.jpg" alt="" id="customer-img"/>
             </div>
             <div>
               <q className="fs-normal fw-light text-muted d-block my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, eius!</q>
-              <h4>Md Akash</h4>
-              <h6 style={{color: '#61CE70'}}>Front End Developer</h6>
+              <h4>Will Smith</h4>
+              <h6 style={{color: '#61CE70'}}>Back End Developer</h6>
             </div>
           </div>
       </SwiperSlide>
       <SwiperSlide className="mb-4">
           <div>
             <div className="customer-img-div">
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" id="customer-img"/>
+              <img src="https://asset1.modelmanagement.com/mm-eyJ0Ijp7InIiOnsibCI6/IjU1MSIsImgiOiI4MDAi/fX0sImlkIjoiaTcxNTk4/NjgiLCJmIjoianBnIn0;.jpg" alt="" id="customer-img"/>
             </div>
             <div>
               <q className="fs-normal fw-light text-muted d-block my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, eius!</q>
-              <h4>Md Akash</h4>
-              <h6 style={{color: '#61CE70'}}>Front End Developer</h6>
+              <h4>Tim Roxet</h4>
+              <h6 style={{color: '#61CE70'}}>UI/UX Developer</h6>
             </div>
           </div>
       </SwiperSlide>
       <SwiperSlide className="mb-4">
           <div>
             <div className="customer-img-div">
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" id="customer-img"/>
+              <img src="https://i.pinimg.com/736x/29/12/d1/2912d1ba7094c8a9ee48455e3be1b002.jpg" alt="" id="customer-img"/>
             </div>
             <div>
               <q className="fs-normal fw-light text-muted d-block my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, eius!</q>
-              <h4>Md Akash</h4>
-              <h6 style={{color: '#61CE70'}}>Front End Developer</h6>
+              <h4>Millie Brown</h4>
+              <h6 style={{color: '#61CE70'}}>Valueable Customer</h6>
             </div>
           </div>
       </SwiperSlide>
       <SwiperSlide className="mb-4">
           <div>
             <div className="customer-img-div">
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" id="customer-img"/>
+              <img src="https://asset1.modelmanagement.com/mm-eyJ0Ijp7InIiOiIzMjAi/fSwiaWQiOiJpMTI1OTM1/MiIsImYiOiJqcGcifQ;;.jpg" alt="" id="customer-img"/>
             </div>
             <div>
               <q className="fs-normal fw-light text-muted d-block my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, eius!</q>
-              <h4>Md Akash</h4>
-              <h6 style={{color: '#61CE70'}}>Front End Developer</h6>
+              <h4>Emily Hanlks</h4>
+              <h6 style={{color: '#61CE70'}}>Customer</h6>
             </div>
           </div>
       </SwiperSlide>
       <SwiperSlide className="mb-4">
           <div>
             <div className="customer-img-div">
-              <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" id="customer-img"/>
+              <img src="https://stylesatlife.com/wp-content/uploads/2021/09/Jennifer-Lawrence-mole-on-face.jpg.webp" alt="" id="customer-img"/>
             </div>
             <div>
               <q className="fs-normal fw-light text-muted d-block my-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic, eius!</q>
-              <h4>Md Akash</h4>
-              <h6 style={{color: '#61CE70'}}>Front End Developer</h6>
+              <h4>Emma Watson</h4>
+              <h6 style={{color: '#61CE70'}}>Customer</h6>
             </div>
           </div>
       </SwiperSlide>
